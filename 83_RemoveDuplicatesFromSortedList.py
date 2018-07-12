30ece004279e600e874a4ed26df8ddce36deb292
Shweta Bharti <shweta.bharti@c02lvbq3fd56.fios-router.home>
2018-07-11 21:54:15 -0400

    second commit

---
+++ 83_RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return []
+        temp = head
+        while temp.next:
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
